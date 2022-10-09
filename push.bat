9cdef2a9672578ace47d2d179a7e7522b77b58b8
AJMC2002 <AJMC2002>
2022-10-09 22:08:53 +1000

    latest commit

--- push.bat
+++ push.bat
@@ -1,10 +1,6 @@
 cd C:\Users\Master\Documents\FEFU\3S\WindowApps
-git init
+git pull
 git add .
-git commit -m "Initial commit"
-git push origin main
-@REM git pull --rebase
-@REM git add .
-@REM git commit -m "latest commit"
-@REM git push -u origin main
+git commit -m "latest commit"
+git push
 pause
