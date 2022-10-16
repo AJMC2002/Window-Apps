8674b796f56d498992dda1f3f57df5e3feb30ecf
AJMC2002 <adrianmosquera2015@gmail.com>
2022-10-16 14:45:21 +1000

    update to main.dart

--- task_1/lib/main.dart
+++ task_1/lib/main.dart
@@ -7,13 +7,24 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Task Manager',
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
+        scaffoldBackgroundColor: Colors.purple[100],
+        listTileTheme: ListTileThemeData(
+          tileColor: Colors.grey[50],
+          contentPadding: const EdgeInsets.all(10),
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(color: Colors.black, width: 1),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        checkboxTheme: const CheckboxThemeData(
+          shape: CircleBorder(),
+        ),
       ),
       home: const MyHomePage(title: 'Studies'),
       debugShowCheckedModeBanner: false,
@@ -31,11 +42,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> _tasks = ['Task 1', 'Task 2', 'Task 3'];
+  final List<String> _taskTitles = ['Task 1', 'Task 2', 'Task 3'];
+  final List<bool?> _taskIsSelected = [false, true, false];
+  final List<bool> _taskIsFav = [false, true, true];
+
+  final _favIcon = const Icon(Icons.star_border_purple500_outlined,
+      color: Colors.deepPurple);
+  final _favIconSelected =
+      const Icon(Icons.star, color: Colors.deepPurpleAccent);
 
   void _addTask() {
     setState(() {
-      _tasks.add(_tasks[_tasks.length-1]+'1');
+      _taskTitles.add(_taskTitles[_taskTitles.length - 1] + '1');
+      _taskIsSelected.add(false);
+      _taskIsFav.add(false);
     });
   }
 
@@ -50,6 +70,35 @@ class _MyHomePageState extends State<MyHomePage> {
       case 'Редактировать тему':
         break;
     }
+  }
+
+  ListTile _newTask(int index) {
+    return ListTile(
+      leading: Checkbox(
+        value: _taskIsSelected[index],
+        onChanged: (bool? newValue) {
+          setState(
+            () {
+              _taskIsSelected[index] = newValue;
+            },
+          );
+        },
+      ),
+      title: Text(
+        _taskTitles[index],
+        style: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+      trailing: IconButton(
+        icon: _taskIsFav[index] ? _favIconSelected : _favIcon,
+        onPressed: () {
+          setState(() {
+            _taskIsFav[index] = !_taskIsFav[index];
+          });
+        },
+      ),
+    );
   }
 
   @override
@@ -77,17 +126,19 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       body: Center(
-        child: ListView.builder(
-          itemCount: _tasks.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(_tasks[index]),
-            );
-          },
+        child: ListView.separated(
+          padding: const EdgeInsets.only(top: 30, left: 30, right: 30),
+          itemCount: _taskTitles.length,
+          itemBuilder: (context, index) => _newTask(index),
+          separatorBuilder: (context, index) => const Divider(
+            color: Colors.purpleAccent,
+            indent: 50,
+            endIndent: 50,
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: _addTask,
         tooltip: 'Add',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
